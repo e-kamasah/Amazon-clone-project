f3a2e54e390ddaa527fb778fca47d1feec2630da
e.kamasah <kamasahedward3@gmail.com>
2025-03-28 02:30:44 +0000

    added backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,10 @@
+new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load',() => {
+   console.log( xhr.response)
+});
+
+
+xhr.open('GET','https://supersimplebackend.dev');
+xhr.send();
