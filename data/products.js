b6e02c3124394cb263b61b2ec4544e0de40fa3ff
e.kamasah <kamasahedward3@gmail.com>
2025-03-30 20:22:48 +0000

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -72,8 +72,33 @@ const product1 = new Product( {
   ]
 });
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
 
 
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -739,3 +764,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
